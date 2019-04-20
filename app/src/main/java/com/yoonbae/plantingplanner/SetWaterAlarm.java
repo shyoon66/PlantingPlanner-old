b796bf1a9dbed5cc93b57124b953baf1b12285b7
oasis <yoosh2002@hanmail.net>
2019-04-20 23:01:17 +0900

    # 핸드폰 재부팅 했을 때 알람 등록

--- app/src/main/java/com/yoonbae/plantingplanner/SetWaterAlarm.java
+++ app/src/main/java/com/yoonbae/plantingplanner/SetWaterAlarm.java
@@ -12,9 +12,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.yoonbae.plantingplanner.service.AlarmService;
 import com.yoonbae.plantingplanner.vo.Plant;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SetWaterAlarm extends AppCompatActivity {
 
@@ -28,17 +32,23 @@ public class SetWaterAlarm extends AppCompatActivity {
         firebaseDatabase.getReference().child("plant").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                //plantList = new ArrayList<>();
-                String fUid = firebaseUser.getUid();
+                if(firebaseUser != null) {
+                    String fUid = firebaseUser.getUid();
 
-                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Plant value = snapshot.getValue(Plant.class);
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        Plant plant = snapshot.getValue(Plant.class);
 
-                    if(value != null) {
-                        String dUid = value.getUid();
-                        if(fUid.equals(dUid)) {
-                            value.setKey(snapshot.getKey());
-                            //plantList.add(value);
+                        if(plant != null) {
+                            String dUid = plant.getUid();
+                            String alarm = plant.getAlarm();
+                            if(fUid.equals(dUid) && "Y".equals(alarm)) {
+                                String alarmDate = plant.getAlarmDate();
+                                String alarmTime = plant.getAlarmTime();
+                                int pod = getPeriod(plant.getPeriod());
+                                String name = plant.getName();
+                                int alarmId = plant.getAlarmId();
+                                setAlarm(alarmDate, alarmTime, pod, name, alarmId);
+                            }
                         }
                     }
                 }
@@ -50,6 +60,63 @@ public class SetWaterAlarm extends AppCompatActivity {
                 Log.w("Hello", "Failed to read value.", databaseError.toException());
             }
         });
+    }
+
+    private void setAlarm(String alarmDate, String alarmTime, int pod, String name, int alarmId) {
+        Map<String, Integer> alarmDateMap = getAlarmDate(alarmDate);
+        Map<String, Integer> alarmTimeMap = getAlarmTime(alarmTime);
+        if(alarmDateMap != null && alarmTimeMap != null) {
+            int year = alarmDateMap.get("year");
+            int month = alarmDateMap.get("month");
+            int dayOfMonth = alarmDateMap.get("dayOfMonth");
+            int hourOfDay = alarmTimeMap.get("hourOfDay");
+            int minute = alarmTimeMap.get("minute");
+
+            LocalDateTime alarmDateTime = LocalDateTime.of(year, month, dayOfMonth, hourOfDay, minute);
+            LocalDateTime nowDateTime = LocalDateTime.now();
+            while(alarmDateTime.isBefore(nowDateTime) || alarmDateTime.isEqual(nowDateTime))
+                alarmDateTime.plusDays(pod);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, month, dayOfMonth, hourOfDay, minute);
+            long alarmTimeInMillis = calendar.getTimeInMillis();
+            long intervalMillis = pod * 24 * 60 * 60 * 1000;
+            AlarmService.INSTANCE.setAlarm(getApplicationContext(), alarmTimeInMillis, intervalMillis, name, alarmId);
+        }
+    }
+
+    private Map<String, Integer> getAlarmDate(String alarmDate) {
+        String[] alarmDateArr = alarmDate.split("-");
+        int year = Integer.parseInt(alarmDateArr[0]);
+        int month = Integer.parseInt(alarmDateArr[1]);
+        int dayOfMonth = Integer.parseInt(alarmDateArr[2]);
+        Map<String, Integer> resultMap = new HashMap<>();
+        resultMap.put("year", year);
+        resultMap.put("month", month);
+        resultMap.put("dayOfMonth", dayOfMonth);
+        return resultMap;
+    }
+
+    private Map<String, Integer> getAlarmTime(String alarmTime) {
+        int hourOfDay = Integer.parseInt(alarmTime.substring(0, alarmTime.indexOf("시")));
+        int minute = Integer.parseInt(alarmTime.substring(alarmTime.indexOf("시") + 2, alarmTime.length() - 1));
+        Map<String, Integer> resultMap = new HashMap<>();
+        resultMap.put("hourOfDay", hourOfDay);
+        resultMap.put("minute", minute);
+        return resultMap;
+    }
+
+    private int getPeriod(String period) {
+        int pod;
+
+        if("매일".equals(period))
+            pod = 1;
+        else if("이틀".equals(period))
+            pod = 2;
+        else
+            pod = Integer.parseInt(period.substring(0, period.length() - 1));
+
+        return pod;
     }
 
 }
